ea29f16d57b149632de4c23a729dbd48cd098aea
vikasassetpanda <vikas@assetpanda.com>
2021-11-16 00:53:29 +0530

    first commit

---
+++ lib/test_plugin.rb
@@ -0,0 +1,6 @@
+require "test_plugin/version"
+require "test_plugin/railtie"
+
+module TestPlugin
+  # Your code goes here...
+end
---
+++ test/test_plugin_test.rb
@@ -0,0 +1,7 @@
+require "test_helper"
+
+class TestPluginTest < ActiveSupport::TestCase
+  test "it has a version number" do
+    assert TestPlugin::VERSION
+  end
+end
